fd2953df901b3cd6185d7ca79be2eff917c0d620
Shijuuten <achmedali@student.ub.ac.id>
2023-11-27 15:25:21 +0700

    ralat nama aplikasi

--- PolarSDK-ECG-HR-Demo/app/src/main/java/com/polar/polarsdkecghrdemo/HistoryActivity.kt
+++ PolarSDK-ECG-HR-Demo/app/src/main/java/com/polar/polarsdkecghrdemo/HistoryActivity.kt
@@ -6,10 +6,16 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class HistoryActivity : AppCompatActivity() {
+    private lateinit var historyManager: HistoryManager
+    private lateinit var historyList: MutableList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
         val textHistory: TextView = findViewById(R.id.textHistory)
+        val textHistory2: TextView = findViewById(R.id.textHistory2)
+
+        historyManager = HistoryManager(this)
+        historyList = historyManager.loadHistory().toMutableList()
 
         // Ambil data ECG dan HR dari penyimpanan
         val ecgData = DataStorage.getECGData()
@@ -21,9 +27,13 @@ class HistoryActivity : AppCompatActivity() {
             textHistory.visibility = View.GONE // Sembunyikan TextView jika ada data
         }
         if (hrData.isEmpty()) {
-            textHistory.visibility = View.VISIBLE // Tampilkan TextView jika tidak ada data
+            textHistory2.visibility = View.VISIBLE // Tampilkan TextView jika tidak ada data
         } else {
-            textHistory.visibility = View.GONE // Sembunyikan TextView jika ada data
+            textHistory2.visibility = View.GONE // Sembunyikan TextView jika ada data
         }
+    }
+    private fun addToHistory(item: String) {
+        historyList.add(item)
+        historyManager.saveHistory(historyList)
     }
 }
--- PolarSDK-ECG-HR-Demo/app/src/main/java/com/polar/polarsdkecghrdemo/HistoryManager.kt
+++ PolarSDK-ECG-HR-Demo/app/src/main/java/com/polar/polarsdkecghrdemo/HistoryManager.kt
@@ -1,4 +1,31 @@
 package com.polar.polarsdkecghrdemo
 
-class HistoryManager {
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class HistoryManager(private val context: Context) {
+    companion object {
+        private const val PREFS_NAME = "HistoryPrefs"
+        private const val HISTORY_KEY = "history_list"
+    }
+
+    private val sharedPreferences: SharedPreferences by lazy {
+        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    }
+
+    fun saveHistory(historyList: List<String>) {
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+        val json = gson.toJson(historyList)
+        editor.putString(HISTORY_KEY, json)
+        editor.apply()
+    }
+
+    fun loadHistory(): List<String> {
+        val json = sharedPreferences.getString(HISTORY_KEY, "")
+        val type = object : TypeToken<List<String>>() {}.type
+        return Gson().fromJson(json, type) ?: listOf()
+    }
 }
